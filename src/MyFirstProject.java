e005377aae03966e4a0e4257b502ccd4dea22c15
aeJOE <takerstone0@gmail.com>
2021-01-13 18:48:15 -0700

    moved to different folder on system.

--- src/MyFirstProject.java
+++ src/MyFirstProject.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class MyFirstProject {
     public static void main(String[] args) throws Exception {
         
-        for(int i=0; i<3; i++ )  {
-            Scanner userInput = new Scanner(System.in); // create scanner object name
-            String output = userInput.nextLine(); // Read user input
+        // for(int i=0; i<3; i++ )  {
+        //     Scanner userInput = new Scanner(System.in); // create scanner object name
+        //     String output = userInput.nextLine(); // Read user input
             
-        }
-        System.out.println("Enter username");
+        // }
+        // System.out.println("Enter username");
     
         System.out.println("My name is Joseph Flores.");
         System.out.println("My favorite color is Pink.");
