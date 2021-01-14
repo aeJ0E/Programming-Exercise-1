136ec34b7d39cec6cb24fda63abcda37a15ac9bb
aeJOE <takerstone0@gmail.com>
2021-01-13 18:39:20 -0700

    edit sys out prompts. incomplete Scanner prompt

---
+++ src/MyFirstProject.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MyFirstProject {
+    public static void main(String[] args) throws Exception {
+        
+        for(int i=0; i<3; i++ )  {
+            Scanner userInput = new Scanner(System.in); // create scanner object name
+            String output = userInput.nextLine(); // Read user input
+            
+        }
+        System.out.println("Enter username");
+    
+        System.out.println("My name is Joseph Flores.");
+        System.out.println("My favorite color is Pink.");
+        System.out.println("If I won the lottery, I would get a bunch of hookers and cocaine.");
+        // This is a meme. I am not being serious. 
+                
+    }
+}
